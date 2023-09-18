85834044aab830691c0290a92b8b6fa5446c61e1
YusuFF <valixonovyusuf01@gmail.com>
2023-09-18 20:03:39 +0500

    added A class

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("Hello,I,m chess playelllr")
+print("Hello,I,m chess player")
+
+
+
+class A:
+
+    def __init__(self, name, number) -> None:
+        self.name = name
+        self.number = number
+
+    def __str__(self) -> str:
+        return f"{self.name}"
+        
